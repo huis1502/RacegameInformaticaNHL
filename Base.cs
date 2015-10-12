4a331ba6c02605fe204236f5147a491feefb85ee
XFXXFX <hindrik1997@hotmail.com>
2015-10-12 12:38:50 +0200

    Main

---
+++ Base.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace RaceGame
+{
+    public static class Base
+    {
+
+
+        public static void Main(string[] Args)
+        {
+
+        }
+    }
+}
---
+++ Window.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace RaceGame
+{
+    public partial class Window : Form
+    {
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Window
+            // 
+            this.ClientSize = new System.Drawing.Size(1008, 747);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(1024, 786);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(1024, 786);
+            this.Name = "Window";
+            this.ShowIcon = false;
+            this.ResumeLayout(false);
+
+        }
+    }
+}
