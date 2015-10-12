98cb92fa96d4c8e666fcbae113ee29614cbe89f9
XFXXFX <hindrik1997@hotmail.com>
2015-10-12 12:10:16 +0200

    First Commit by Hindrik1997

---
+++ Base.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace RaceGame
+{
+    public static class Base
+    {
+        public static Window windowHandle;
+
+
+
+
+
+        public static void Main(string[] args)
+        {
+            windowHandle = new Window();
+            Application.Run(windowHandle);
+        }
+
+
+
+    }
+}
---
+++ Window.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace RaceGame
+{
+    public partial class Window : Form
+    {
+        public Window()
+        {
+            
+        }
+
+        private void Window_Load(object sender, EventArgs e) { }
+
+    }
+}
