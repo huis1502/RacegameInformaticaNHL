dd3a182ffd3ed90b1a24ed29f30a624a23c7c3c6
XFXXFX <hindrik1997@hotmail.com>
2015-10-12 12:17:40 +0200

    AutoSize update

--- Window.cs
+++ Window.cs
@@ -10,7 +10,21 @@ namespace RaceGame
             
         }
 
-        private void Window_Load(object sender, EventArgs e) { }
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Window
+            // 
+            this.ClientSize = new System.Drawing.Size(1008, 747);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(1024, 786);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(1024, 786);
+            this.Name = "Window";
+            this.ShowIcon = false;
+            this.ResumeLayout(false);
 
+        }
     }
 }
