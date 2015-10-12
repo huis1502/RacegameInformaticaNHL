253837e92d56ec38597fcc7b6927eaa38618e9fd
XFXXFX <hindrik1997@hotmail.com>
2015-10-12 14:50:15 +0200

    Mainloop functional including insertion system for DrawInfo's and GameTask's

---
+++ DelegatesAndEnums.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Drawing;
+
+namespace RaceGame.Delegates
+{
+    public delegate void GameTask();
+
+}
+
+namespace RaceGame.Enums
+{
+    
+}
+
+namespace RaceGame.Structs
+{
+    public struct DrawInfo
+    {
+        public Bitmap bitmapdata;
+        public int x;
+        public int y;
+        public int width;
+        public int height;
+        public bool AutoRemove;
+        public int Frames;
+
+        public DrawInfo(Bitmap bitmap, int x, int y, int width, int height, bool AutoRemove = false, int Frames = 0)
+        {
+            bitmapdata = bitmap;
+            this.x = x;
+            this.y = y;
+            this.width = width;
+            this.height = height;
+            this.AutoRemove = AutoRemove;
+            this.Frames = Frames;
+        }
+        public void LowerFrameCount()
+        {
+            Frames = Frames - 1;
+        }
+    }
+}
--- Window.cs
+++ Window.cs
@@ -1,15 +1,37 @@
 ﻿using System;
+using System.Drawing;
 using System.Windows.Forms;
+using System.Collections.Generic;
+using RaceGame.Structs;
+using RaceGame.Delegates;
 
 namespace RaceGame
 {
     public partial class Window : Form
     {
+        public Timer GameTimer;
+        public Bitmap BackBuffer;
+        public List<GameTask> GameTasks;
+        public List<DrawInfo> DrawInfos;
+
+        public static Bitmap bitje = new Bitmap("C:/Users/Hindrik/Documents/Test.bmp");
+        public static DrawInfo draw = new DrawInfo(bitje,50,50,104,104, true, 120);
+
         public Window()
         {
             InitializeComponent();
-        }
+            GameTasks = new List<GameTask>();
+            DrawInfos = new List<DrawInfo>();
+            DrawInfos.Add(draw);
 
+            SetStyle(ControlStyles.UserPaint | ControlStyles.AllPaintingInWmPaint | ControlStyles.DoubleBuffer, true);
+            GameTimer = new Timer();
+            GameTimer.Interval = 16;
+            GameTimer.Tick += new EventHandler(GameTimer_Tick);
+            GameTimer.Start();
+            Load += new EventHandler(CreateBackBuffer);
+            Paint += new PaintEventHandler(PaintBackbuffer);
+        }
         private void InitializeComponent()
         {
             this.SuspendLayout();
@@ -23,8 +45,62 @@ namespace RaceGame
             this.MinimumSize = new System.Drawing.Size(1024, 786);
             this.Name = "Window";
             this.ShowIcon = false;
+            this.Text = "Race Game";
             this.ResumeLayout(false);
 
         }
+        void GameTimer_Tick(object sender, EventArgs e)
+        {
+            //Alle game tasks invoken
+            for (int i = 0; i < GameTasks.Count; i++)
+            {
+                GameTasks[i].Invoke();
+            }
+            Draw();
+        }
+        void Draw()
+        {
+            if (BackBuffer != null)
+            {
+                using (var buffer = Graphics.FromImage(BackBuffer))
+                {
+                    buffer.Clear(Color.LightGray);
+                    for (int i = 0; i < DrawInfos.Count;i++)
+                    {
+                        DrawInfo DR = DrawInfos[i];
+                        buffer.DrawImage(DR.bitmapdata, DR.x, DR.y, DR.width, DR.height);
+                        if (DR.AutoRemove)
+                        {
+                            if (DR.Frames != 0)
+                            {
+                                DrawInfos[i] = new DrawInfo(DR.bitmapdata, DR.x, DR.y,DR.width,DR.height,DR.AutoRemove,DR.Frames - 1);
+                            }
+                            else
+                            {
+                                DrawInfos.RemoveAt(i);
+                            }
+                        }
+                    }                  
+                }
+                Invalidate();
+            }
+        }
+        void CreateBackBuffer(object sender, EventArgs e)
+        {
+            if (BackBuffer != null)
+            {
+                BackBuffer.Dispose();
+            }
+            BackBuffer = new Bitmap(ClientSize.Width, ClientSize.Height);
+        }
+        void PaintBackbuffer(object sender, PaintEventArgs e)
+        {
+            if (BackBuffer != null)
+            {
+                e.Graphics.DrawImageUnscaled(BackBuffer, new Point(0,0));
+            }
+        }
+
+
     }
 }
