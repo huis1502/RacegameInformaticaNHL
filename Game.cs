da8917f1d92cf0b53ce38f2373e0677c8aa94776
Hindrik1997 <hindrik1997@hotmail.com>
2015-10-17 19:25:31 +0200

    Procedural Generation step 1

--- Game.cs
+++ Game.cs
@@ -11,7 +11,7 @@ namespace RaceGame
         public Player player2;
         public byte[,] GameField;
         int MapsizeX = 1008;
-        int MapsizeY = 747;
+        int MapsizeY = 9*72;
         public Bitmap Background;
         protected Random random;
         public System.Drawing.Point[] Points;
@@ -48,87 +48,27 @@ namespace RaceGame
         public void CreateMap()
         {
             List<Point> Points = new List<Point>();
-            int SegmentWidth = 100;
-            for (int x = 0; x < (MapsizeX / SegmentWidth); x++ )
+            int SegmentWidth = 72;
+            for (int x = 0; x < (MapsizeX / SegmentWidth); x++)
             {
                 for (int y = 0; y < (MapsizeY / SegmentWidth); y++)
-                {
-                    if ( (x == 1 || y == 1 || x == (MapsizeX / SegmentWidth - 2) || y == (MapsizeY / SegmentWidth - 2) ) && x != 0 && y != 0 && x != (MapsizeX/SegmentWidth-1) && y != (MapsizeY/SegmentWidth-1) )
-                    {
-                        PointType pointtype;
-                        if (x == 1 &&  y == 1 || x==1 && y == MapsizeY / SegmentWidth - 2 ||  x == MapsizeX / SegmentWidth - 2 && y == 1 || x == MapsizeX / SegmentWidth - 2 && y == MapsizeY / SegmentWidth - 2)
-                        {
-                            pointtype = PointType.Corner;
-                        }
-                        else
-                        {
-                            pointtype = PointType.Inner;
-                        }
-
-                        int MidpointX = x * SegmentWidth + (SegmentWidth / 2);
-                        int MidpointY = y * SegmentWidth + (SegmentWidth / 2);
-
-                        if (pointtype == PointType.Inner)
-                        {
-                            int xpos = random.Next(-10,10);
-                            int ypos = random.Next(-10, 10);
-                            MidpointX += xpos;
-                            MidpointY += ypos;
-                        }
-                        Points.Add(new Point(pointtype, MidpointX, MidpointY));
-                    }
+                {   
+                    int MidpointX = x * SegmentWidth + (SegmentWidth / 2);
+                    int MidpointY = y * SegmentWidth + (SegmentWidth / 2);
+                    Points.Add(new Point(MidpointX, MidpointY));
                 }
             }
+
             for (int i = 0; i < Points.Count; i++)
             {
-                GameField[Points[i].x, Points[i].y] = 255;
-            }
-            Queue<Point> PointQueue = new Queue<Point>();
-            PointQueue.Enqueue(Points[0]);
-            List<Point> PointsDone = new List<Point>();
-            while (PointQueue.Count != 0)
-            {
-                Point P = PointQueue.Dequeue();
-                float MinDistance = -1;
-                Point NextPoint = null;
-                for (int j = 0; j < Points.Count; j++)
-                {
-                    if (P != Points[j] && !PointsDone.Contains(Points[j]))
-                    {
-                        float Distance = CalculateDistance(P, Points[j]);
-                        if (MinDistance == -1 || MinDistance > Distance)
-                        {
-                            MinDistance = Distance;
-                            NextPoint = Points[j];
-                        }
-                    }
-                }
-                if (NextPoint != null)
-                {
-                    PointsDone.Add(NextPoint);
-                    PointQueue.Enqueue(NextPoint);
-                }
-            }
-            PointsDone.Add(PointsDone[0]);
-
-            for (int i = 0; i < 5; i++)
-            {
-                PointsDone = CatMullRom(PointsDone);
-            }
-
-            for (int i = 0; i < PointsDone.Count; i++)
-            {
-                Console.WriteLine(PointsDone[i].x + "-" + PointsDone[i].y);
-                int x = PointsDone[i].x;
-                int y = PointsDone[i].y;
+                int x = Points[i].x;
+                int y = Points[i].y;
 
                 if (x >= 0 && y >= 0 && x < MapsizeX && y < MapsizeY)
                 {
-                    GameField[PointsDone[i].x, PointsDone[i].y] = 255;
+                    GameField[Points[i].x, Points[i].y] = 255;
                 }
             }
-
-            Points = PointsDone;
             Background = new Bitmap(MapsizeX, MapsizeY);
             for (int x = 0; x < MapsizeX; x++)
             {
@@ -143,14 +83,10 @@ namespace RaceGame
                     {
                         color = Color.White;
                     }
-                    Background.SetPixel(x,y, color);
+                    Background.SetPixel(x, y, color);
                 }
             }
-
-
-            //Base.drawInfos.Add(new Structs.DrawInfo(Background,MapsizeX/2,MapsizeY/2,MapsizeX,MapsizeY));
-
-            Base.drawInfos.Add(new Structs.DrawInfo(Background, MapsizeX/2, MapsizeY/2, MapsizeX, MapsizeY, 270, 0));
+            Base.drawInfos.Add(new Structs.DrawInfo(Background, MapsizeX / 2, MapsizeY / 2, MapsizeX, MapsizeY, 270, 0));
             this.Points = new System.Drawing.Point[Points.Count];
             for (int i = 0; i < Points.Count; i++)
             {
@@ -158,74 +94,15 @@ namespace RaceGame
                 this.Points[i] = p;
             }
         }
-
         public float CalculateDistance(Point a, Point b)
         {
             return (float)Math.Sqrt(Math.Pow(b.x -a.x, 2) + Math.Pow(b.y - a.y,2));
         }
 
-        public List<Point> CatMullRom(List<Point> _points)
-        {
-            int OriginalPointsCount = _points.Count;
-
-            for (int i = 0; i < OriginalPointsCount * 2; i += 2)
-            {
-                Point zero = _points[i];
-                Point one;
-                Point two;
-                Point three;
-
-                #region IFs
-                if (i + 1 == _points.Count)
-                {
-                    //+1
-                    one = _points[0];
-                    two = _points[1];
-                    three = _points[2];
-                }
-                else
-                {
-                    if (i + 2 == _points.Count)
-                    {
-                        one = _points[i + 1];
-                        two = _points[0];
-                        three = _points[1];
-                    }
-                    else
-                    {
-                        if (i + 3 == _points.Count)
-                        {
-                            one = _points[i + 1];
-                            two = _points[i + 2];
-                            three = _points[0];
-                        }
-                        else
-                        {
-                            one = _points[i + 1];
-                            two = _points[i + 2];
-                            three = _points[i + 3];
-                        }
-                    }
-                }
-                #endregion
-
-                Point X;
-                var u = 0.5f;
-                var u3 = u * u * u;
-                var u2 = u * u;
-                var f1 = -0.5 * u3 + u2 - 0.5 * u;
-                var f2 = 1.5 * u3 - 2.5 * u2 + 1.0;
-                var f3 = -1.5 * u3 + 2.0 * u2 + 0.5 * u;
-                var f4 = 0.5 * u3 - 0.5 * u2;
-                var x = zero.x * f1 + one.x * f2 + two.x * f3 + three.x * f4;
-                var y = zero.y * f1 + one.y * f2 + two.y * f3 + three.y * f4;
 
 
-                X = new Point(PointType.Inner, (int)x,(int)y);
-                _points.Insert(_points.IndexOf(two) + 1, X);
-            }
-            return _points; ;
-        }
+
+
 
     }
 }
--- Point.cs
+++ Point.cs
@@ -4,13 +4,11 @@ namespace RaceGame
 {
     public class Point
     {
-        PointType pointType;
         public int x;
         public int y;
 
-        public Point(PointType p, int _x, int _y)
+        public Point(int _x, int _y)
         {
-            pointType = p;
             x = _x;
             y = _y;
         }
